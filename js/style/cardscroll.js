2ac128c4349bdcc1e901de53e56ab7cad3b6e232
makimafox <fluke.kukeinoob@gmail.com>
2022-10-28 17:44:41 +0700

    refactor and new design
    rename css file add card scrolljs

---
+++ js/style/cardscroll.js
@@ -0,0 +1,11 @@
+function left() {
+
+    document.querySelector(".container-card").scrollBy(-200,0)
+    // console.log("left")
+}
+function right() {
+    document.querySelector(".container-card").scrollBy(200,0)
+    // console.log("right")
+}
+
+// card scroll function file 
--- js/style/scroll.js
+++ js/style/scroll.js
@@ -2,4 +2,11 @@ window.addEventListener("scroll",function(){
 
     let scroll = this.scrollY
     // console.log(scroll)
+    if (scroll<5){
+
+        document.getElementById("header").style.backgroundColor = 'transparent'
+    }else {
+
+        document.getElementById("header").style.backgroundColor = "rgb("+255+","+255+","+255+")"
+    }
 });
