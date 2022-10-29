6bfd1d8df9a9668e52d6db4d625a091705a42564
walnu-t <palmjimmod@gmail.com>
2022-10-29 21:02:43 +0700

    header
    add transition and simple hover effect<might add more soon>

--- js/style/scroll.js
+++ js/style/scroll.js
@@ -3,10 +3,13 @@ window.addEventListener("scroll",function(){
     let scroll = this.scrollY
     // console.log(scroll)
     if (scroll<5){
-
-        document.getElementById("header").style.backgroundColor = 'transparent'
+        document.getElementById("header").style.backgroundColor = 'transparent';
+        document.getElementById("header").style.boxShadow = "none";
     }else {
-
-        document.getElementById("header").style.backgroundColor = "rgb("+255+","+255+","+255+")"
+        document.getElementById("header").style.backgroundColor = "rgba("+254+","+254+","+254+","+0.9+")";
+        document.getElementById("header").style.boxShadow = "0px 4px 10px rgba("+0+","+0+","+0+","+0.1+")"
     }
 });
+
+// background: rgba(254, 254, 254, 0.9);
+// box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
