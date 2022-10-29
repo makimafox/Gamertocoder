a55380099897576671d0694adafb14134b7fc0d0
walnu-t <palmjimmod@gmail.com>
2022-10-29 14:57:24 +0700

    add game page
    added game.html and game.css also added some for of loop in api minigames

--- js/api/api-minigame.js
+++ js/api/api-minigame.js
@@ -17,9 +17,28 @@ fetch("https://gamertocoder.garena.co.th/api/minigames")
             document.getElementById("gamename").appendChild(newListItem)
             
         }
-
-        
-
+        for(let currentData of data_game) {
+            const {genre, name, icon, description} = currentData;
+      
+            const newDivItem = document.createElement("div");
+            newDivItem.classList.add("card");
+            const genre_array = genre;
+            let genre_string = genre_array.join()
+            let html =`
+            <div class="center">
+              <p onclick="changeName('${name}')"><strong>ชื่อ: ${name}</strong></p>
+              <img src="${icon}"/>
+            </div>
+            <p><strong>ประเภท:</strong> ${genre_string}</p><br>
+            <p class="detail">${description}</p><br>
+            <div class="center view-image">
+              <form action="${icon}" method="get" target="_blank">
+                  <button class="button" type="submit">View Image</button>
+              </form>
+            </div>`;
+            html = html.trim();
+            newDivItem.innerHTML = html;
+            document.getElementById("list").appendChild(newDivItem);
+          }
     }
 })
-
